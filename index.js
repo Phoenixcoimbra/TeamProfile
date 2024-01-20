29530906f63c87c30493378dcae3f4e041c15f89
Phoenixcoimbra <igormcoimbra@gmail.com>
2024-01-20 00:05:37 +0000

    changed import statment to require

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
@@ -5,9 +6,11 @@ const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
 
+
+
+
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
-
 const render = require("./src/page-template.js");
 const { create } = require("domain");
 
@@ -177,6 +180,7 @@ function addEngineer() {
         createTeam();
     });
 
+}
 // function to add an intern
 
 function addIntern() {
@@ -255,4 +259,10 @@ function addIntern() {
 
 appMenu();
 createManager();
+createTeam();
+addEngineer();
+addIntern();
+buildTeam();
 
+
+}
